8d63ededa58bb0f6551b4ab0e31a22ac9842af6d
Noam <noam@fireflymigration.com>
2019-03-13 17:37:17 +0200

    Fixed semantics

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -10,43 +10,43 @@ import { environment } from '../../environments/environment';
 })
 export class HomeComponent {
   selectCustomerGrid = new radweb.GridSettings(new models.Customers(),
-  {
-    
-    numOfColumnsInGrid:4,
-    columnSettings: customers => [
-      customers.id,
-      customers.companyName,
-      customers.contactName,
-      customers.country,
-      customers.address,
-      customers.city
-    ]
-  });
+    {
+
+      numOfColumnsInGrid: 4,
+      columnSettings: customers => [
+        customers.id,
+        customers.companyName,
+        customers.contactName,
+        customers.country,
+        customers.address,
+        customers.city
+      ]
+    });
   ordersGrid = new radweb.GridSettings<models.Orders>(new models.Orders(),
     {
-      get:{
-        limit:25
+      get: {
+        limit: 25
       },
-      knowTotalRows:true,
+      knowTotalRows: true,
       numOfColumnsInGrid: 4,
       allowUpdate: true,
-      
+
       allowInsert: true,
       onEnterRow: orders =>
-      this.orderDetailsGrid.get({
-        where: orderDetails =>
-          orderDetails.orderID.isEqualTo(orders.id),
-          limit:50
-      }),
+        this.orderDetailsGrid.get({
+          where: orderDetails =>
+            orderDetails.orderID.isEqualTo(orders.id),
+          limit: 50
+        }),
       columnSettings: orders => [
         {
           column: orders.id,
-          width:'90px',
+          width: '90px',
           readonly: true,
         },
         {
           column: orders.customerID,
-          width:'400px',
+          width: '400px',
           getValue: orders =>
             orders.lookup(new models.Customers(), orders.customerID).companyName,
           click: orders =>
@@ -54,14 +54,18 @@ export class HomeComponent {
               selectedCustomer =>
                 orders.customerID.value = selectedCustomer.id.value)
         },
-        orders.orderDate,
+        {
+          column: orders.orderDate,
+          width:'170px'
+        },
         {
           column: orders.shipVia,
+          width: '150px',
           dropDown: {
             source: new models.Shippers()
           }
         },
-     
+
       ],
       rowButtons: [
         {
@@ -74,7 +78,7 @@ export class HomeComponent {
     }
   );
   shipInfoArea = this.ordersGrid.addArea({
-    numberOfColumnAreas:2,
+    numberOfColumnAreas: 2,
     columnSettings: orders => [
       orders.requiredDate,
       orders.shippedDate,
@@ -83,32 +87,38 @@ export class HomeComponent {
     ]
   });
   orderDetailsGrid = new radweb.GridSettings<models.OrderDetails>(new models.OrderDetails(),
-  {
-    allowUpdate: true,
-    allowDelete: true,
-    allowInsert: true,
-    knowTotalRows:true,
-    columnSettings: order_details => [
-      {
-        column: order_details.productID,
-        dropDown: {
-          source: new models.Products()
+    {
+      allowUpdate: true,
+      allowDelete: true,
+      allowInsert: true,
+      knowTotalRows: true,
+      columnSettings: order_details => [
+        {
+          column: order_details.productID,
+          width: '300px',
+          dropDown: {
+            source: new models.Products()
+          }
+        }, {
+          column: order_details.unitPrice,
+          width: '100px'
+        },
+        {
+          column: order_details.quantity, width: '100px'
+        },
+        {
+          caption: 'Total',
+          width: '100px',
+          getValue: orderDetails =>
+            (orderDetails.quantity.value * orderDetails.unitPrice.value).toFixed(2)
         }
+      ],
+      onNewRow: orderDetail => {
+        orderDetail.orderID.value = this.ordersGrid.currentRow.id.value;
+        orderDetail.quantity.value = 1;
       },
-      order_details.unitPrice,
-      order_details.quantity,
-      {
-        caption: 'Total',
-        getValue: orderDetails =>
-          (orderDetails.quantity.value * orderDetails.unitPrice.value).toFixed(2)
-      }
-    ],
-    onNewRow: orderDetail => {
-      orderDetail.orderID.value = this.ordersGrid.currentRow.id.value;
-      orderDetail.quantity.value = 1;
-    },
-  
-  });
+
+    });
   getOrderTotal() {
     let result = 0;
     this.orderDetailsGrid.items.forEach(
@@ -120,5 +130,5 @@ export class HomeComponent {
     window.open(
       environment.serverUrl + 'home/print/' + this.ordersGrid.currentRow.id.value);
   }
-  
+
 }
