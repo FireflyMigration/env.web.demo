06dd41c7053439cdbc15b74d1d3e3ecd40c947b8
Noam Honig <noam.honig@gmail.com>
2024-05-13 13:46:50 +0300

    Added react

---
+++ react/vite.config.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: { proxy: { '/dataApi': 'http://localhost:56557' } },
+  build: { outDir: '../mvc/frontend-dist-files' },
+})
