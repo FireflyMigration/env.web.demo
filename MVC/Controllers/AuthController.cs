a1b5655ed879aac9b1cc8b6b4ee45444e18f970f
Noam Honig <noam.honig@gmail.com>
2024-12-26 07:58:42 +0200

    added auth controller and jsosStringHelpers

---
+++ MVC/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public ActionResult Login(string userName, string password)
+        {
+            var userInfo = new JwtUserInfo(userName, userName, "Admin", "Login");
+            Response.SetCookie(new System.Web.HttpCookie("Authorization", MvcApplication.Jwt.GetToken(userInfo))
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = System.Web.SameSiteMode.Strict
+            });
+            return Json(userInfo);
+        }
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Response.SetCookie(new System.Web.HttpCookie("Authorization", "")
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = System.Web.SameSiteMode.Strict
+            });
+            return Json("ok");
+        }
+        public ActionResult Me()
+        {
+            if (JwtUserInfo.CurrentUser.Value == null)
+                return Json(null, JsonRequestBehavior.AllowGet);
+
+            return Json(JwtUserInfo.CurrentUser.Value, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
